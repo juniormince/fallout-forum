f4ccf35bfd1536de92f0df86d9771d5a2c16d286
juniormince <groundcontroltomajorbree@gmail.com>
2018-06-11 17:32:17 -0500

    add query priorities

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 // reorganize route list after completed
-
+// ******** = incomplete/not functional
 
 /// GET profile [DONE]
 router.get('/profile/:id', (req, res) => {
@@ -63,46 +63,6 @@ router.post('/newReply', (req, res) => {
     }
 });
 
-// ******** GET profile settings
-router.get('/profile/:id', (req, res) => {
-    console.log('GET all route for user', req.params.id);
-    if (req.isAuthenticated()) 
-        {
-        let queryText = `SELECT * FROM "person" WHERE "id" = $1;`;
-        pool.query(queryText, [req.user.id])
-        .then((result) => {
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log('error on person GET: ', error);
-            res.sendStatus(500);
-        })
-    } 
-    else {
-        res.sendStatus(403);
-    }
-});
-
-// ******** PUT edit profile settings
-router.put('/:id', (req, res) => {
-    console.log('PUT item route');
-    if (req.isAuthenticated() && req.params.id === req.user.id) {
-        let queryText = `UPDATE "person" SET "profile_alias" = $2, "profile_location" = $3, 
-                        "profile_timezone" = $4, "profile_contact" = $5, "profile_img" = $6
-                        WHERE "id" = $1`;
-        pool.query(queryText, [req.user.id, req.params.profile_alias, req.params.profile_location, 
-                            req.params.profile_timezone, req.params.profile_contact, req.params.profile_img])
-        .then((result) => {
-            res.sendStatus(200)
-        })
-        .catch((error) => {
-            console.log('error on PUT: ', error)
-            res.sendStatus(500);
-        })
-    } else {
-        res.sendStatus(403);
-    }
-
-});
 
 
 // ******** GET threads by id TEST
@@ -134,8 +94,32 @@ router.put('/:id', (req, res) => {
 //     }
 // });
 
+// ******** GET threads by topic id
+// router.get('/threads', (req, res) => {
+//     console.log('GET all threads by topic id');
+//     if(req.isAuthenticated()) {
+//         // let queryText = 
+//         //                 `SELECT "thread"."title", "thread"."id"
+//         //                 FROM "---"
+//         //                 RIGHT JOIN "----" ON "---"."_id" = "----"."id"
+//         //                 GROUP BY "thread"."title"
+//         //                 ORDER BY count DESC`;
+//         pool.query(queryText)
+//         .then((result) => {
+//             res.send(result.rows);
+//         })
+//         .catch((error) => {
+//             console.log('error on GET /threads: ', error);
+//             res.sendStatus(500);
+//         })
+//     } 
+//     else {
+//         res.sendStatus(403);
+//     }
+// });
 
-// ******** GET threads TEST
+
+// ******** GET threads DRAFT (all)
 router.get('/threads', (req, res) => {
     console.log('GET all route for thread');
     if (req.isAuthenticated() 
@@ -204,30 +188,6 @@ router.get('/comments', (req, res) => {
 
 
 
-// ******** GET list of threads by topic id
-// router.get('/threads', (req, res) => {
-//     console.log('GET all threads by topic id');
-//     if(req.isAuthenticated()) {
-//         // let queryText = 
-//         //                 `SELECT "thread"."title", "thread"."id"
-//         //                 FROM "---"
-//         //                 RIGHT JOIN "----" ON "---"."_id" = "----"."id"
-//         //                 GROUP BY "thread"."title"
-//         //                 ORDER BY count DESC`;
-//         pool.query(queryText)
-//         .then((result) => {
-//             res.send(result.rows);
-//         })
-//         .catch((error) => {
-//             console.log('error on GET /threads: ', error);
-//             res.sendStatus(500);
-//         })
-//     } 
-//     else {
-//         res.sendStatus(403);
-//     }
-// });
-
 
 // ******** POST route template (new thread)
 // router.post('/addthread', (req, res) => {
@@ -248,26 +208,46 @@ router.get('/comments', (req, res) => {
 // });
 
 
-// ******** POST route template (new reply/comment)
-router.post('/newReply', (req, res) => {
-    if (req.isAuthenticated()) {
-        let queryText = `INSERT INTO "comment" ("reply", "person_id")
-                        VALUES ($1, $2)`;
-        pool.query(queryText, [req.body.reply, req.user.id])
-            .then((result) => {
-                res.sendStatus(201);
-            })
-            .catch((error) => {
-                res.sendStatus(500);
-                console.log('error on POST: ', error)
-            })
-    } else {
+// ******** GET profile settings
+router.get('/profile/:id', (req, res) => {
+    console.log('GET all route for user', req.params.id);
+    if (req.isAuthenticated()) 
+        {
+        let queryText = `SELECT * FROM "person" WHERE "id" = $1;`;
+        pool.query(queryText, [req.user.id])
+        .then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log('error on person GET: ', error);
+            res.sendStatus(500);
+        })
+    } 
+    else {
         res.sendStatus(403);
     }
 });
 
+// ******** PUT edit profile settings
+router.put('/:id', (req, res) => {
+    console.log('PUT item route');
+    if (req.isAuthenticated() && req.params.id === req.user.id) {
+        let queryText = `UPDATE "person" SET "profile_alias" = $2, "profile_location" = $3, 
+                        "profile_timezone" = $4, "profile_contact" = $5, "profile_img" = $6
+                        WHERE "id" = $1`;
+        pool.query(queryText, [req.user.id, req.params.profile_alias, req.params.profile_location, 
+                            req.params.profile_timezone, req.params.profile_contact, req.params.profile_img])
+        .then((result) => {
+            res.sendStatus(200)
+        })
+        .catch((error) => {
+            console.log('error on PUT: ', error)
+            res.sendStatus(500);
+        })
+    } else {
+        res.sendStatus(403);
+    }
 
-
+});
 
 /**
  * DELETE route template (delete comments)
