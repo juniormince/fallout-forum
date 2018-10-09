9ca26302cffeb57893d8ff961e21f969b6608de8
juniormince <groundcontroltomajorbree@gmail.com>
2018-10-08 19:51:35 -0500

    spacing edit

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { triggerLogout } from '../../redux/actions/loginActions';
